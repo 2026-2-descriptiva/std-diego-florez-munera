3a7f81a075eea4fd581df2c62b43f74a0d100fa4
diflorezm-maker <diflorezm@unal.edu.co>
2026-09-25 14:02:21 -0500

    avance

--- PRE_04_limpieza/src/diagnostics.py
+++ PRE_04_limpieza/src/diagnostics.py
@@ -5,7 +5,12 @@ OUTPUT_FILE = "PRE_04_limpieza/submission/ventas.csv"
 
 def main():
     df = pd.read_csv(OUTPUT_FILE)
-    series = df["supplier"]
+
+    series = df["amount"]
+
+    series = series.astype(str)
+    # series = series[series.str.contains(r"K$", regex=True)]
+
     series = series.sort_values()
     series = series.drop_duplicates()
 
